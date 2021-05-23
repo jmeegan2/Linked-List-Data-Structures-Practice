874a6efb785408a4736a7f66421977ead10161de
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-05-23 19:58:32 -0400

    Done with this practice code for data structures

--- Source.cpp
+++ Source.cpp
@@ -1,69 +1,46 @@
+// A simple C++ program for traversal of a linked list
+#include <vector>
 #include <iostream>
 using namespace std;
 
-struct Node
-{
+class Node {
+public: 
 	int data;
-
 	Node* next;
-
 };
-
-//only for the 1st Node
-void initNode(struct Node* head, int n) {
-	head->data = n;
-	head->next = NULL;
-}
-
-//apending
-void addNode(struct Node* head, int n) {
-	Node *newNode = new Node;
-	newNode->data = n;
-	newNode->next = NULL;
-
-	Node* cur = head;
-		while (cur) {
-			if (cur->next == NULL) {
-				cur->next = newNode;
-				return;
-			}
-			cur = cur->next;
-		}
-}
-void insertFront(struct Node **head, int n) {
-	Node *newNode = new Node;
-	newNode->data = n;
-	newNode->next = *head;
-	*head = newNode;
-}
-
-struct Node* searchNode(struct Node* head, int n) {
-	Node *cur = head;
-	while (cur) {
-		if (cur-> data == n) return cur;
-		cur = cur->next;
+//This function prints contents of linked list
+//starting from the given node
+void printList(Node* n)
+{
+	while (n != NULL) {
+		cout << n->data << " ";
+		n = n->next;
 	}
-	cout << "No Node " << n << " in list.\n";
+}
+//Driver code
+int main() {
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+
+	// allocate 3 nodes in the heap
+	head = new Node();
+	second = new Node();
+	third = new Node();
+
+	head->data = 1; //assign data in first node
+	head->next = second; //Link first node with second 
+
+	second->data = 2; //assign data to second node 
+	second->next = third;
+
+	third->data = 3; //assign data to third node 
+	third->next = NULL;
+
+	printList(head);
+
+	return 0;
+
 
 }
-
-bool deleteNode(struct Node** head, Node* ptrDel) {
-	Node* cur = *head;
-	if (ptrDel == *head) {
-		*head = cur->next;
-		delete ptrDel;
-		return true;
-	}
-	while (cur) {
-		if (cur->next == ptrDel) {
-			cur->next = ptrDel->next;
-			delete ptrDel;
-			return true;
-		}
-		cur = cur->next;
-	}
-	return false;
-}
-/* reverse the list*/
-struct Node* reve
-
+//Just some data structure practice that I found online
